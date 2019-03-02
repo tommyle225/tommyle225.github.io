be52b890454333437ef649c99c9a2118244b86af
Tommy Le <tommyle225@gmail.com>
2019-03-01 19:33:05 -0600

    Updates

---
+++ precache-manifest.fbd3b36dcbddf190e1b8f379bc96f0f8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "462806316fea535a6a57651bc2b000b0",
+    "url": "/portfolio/static/media/fa-solid-900.46280631.woff2"
+  },
+  {
+    "revision": "d8566345c44be0282c02",
+    "url": "/portfolio/static/css/main.5a6f7384.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/portfolio/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "8c32a398cb1127be932aa922fb468fa5",
+    "url": "/portfolio/static/media/profile.8c32a398.jpg"
+  },
+  {
+    "revision": "65779ebcc35604a25c2ba77309c5b8af",
+    "url": "/portfolio/static/media/fa-regular-400.65779ebc.woff2"
+  },
+  {
+    "revision": "5dd3976cb5d61e2e561f2a46b916f377",
+    "url": "/portfolio/static/media/fa-regular-400.5dd3976c.woff"
+  },
+  {
+    "revision": "414ff5daad323a1c47c5177d4bd29674",
+    "url": "/portfolio/static/media/fa-regular-400.414ff5da.eot"
+  },
+  {
+    "revision": "f6c6f6c8cb7784254ad00056f6fbd74e",
+    "url": "/portfolio/static/media/fa-regular-400.f6c6f6c8.ttf"
+  },
+  {
+    "revision": "4b115e1153a9ea339d6a0bb284cc8ed3",
+    "url": "/portfolio/static/media/fa-brands-400.4b115e11.woff2"
+  },
+  {
+    "revision": "d8566345c44be0282c02",
+    "url": "/portfolio/static/js/main.d8566345.chunk.js"
+  },
+  {
+    "revision": "61969d433bf265b9717a6c357a1e04e4",
+    "url": "/portfolio/static/media/fa-solid-900.61969d43.woff"
+  },
+  {
+    "revision": "b90365bccdabd68c6c03902b4b141f09",
+    "url": "/portfolio/static/media/fa-brands-400.b90365bc.woff"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/portfolio/static/media/fa-brands-400.c39278f7.ttf"
+  },
+  {
+    "revision": "d9d17590c975aad1be0ddab673f9c769",
+    "url": "/portfolio/static/media/fa-brands-400.d9d17590.eot"
+  },
+  {
+    "revision": "e7e957c87c454bccaa3bf9fdaa6874f8",
+    "url": "/portfolio/static/media/fa-regular-400.e7e957c8.svg"
+  },
+  {
+    "revision": "b5596f4d339f99e3d69bc41be78db962",
+    "url": "/portfolio/static/media/fa-solid-900.b5596f4d.eot"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/portfolio/static/media/fa-solid-900.b70cea03.ttf"
+  },
+  {
+    "revision": "80533988ff5fecd5be26557d08ce8237",
+    "url": "/portfolio/static/media/fa-brands-400.80533988.svg"
+  },
+  {
+    "revision": "82905d8d1c06969df11c8c378e9bdd4c",
+    "url": "/portfolio/static/media/fa-solid-900.82905d8d.svg"
+  },
+  {
+    "revision": "6915db1b32bb353cecf854735f2c1452",
+    "url": "/portfolio/index.html"
+  }
+];
